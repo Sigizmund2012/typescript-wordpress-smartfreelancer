9de15e6bed7c8342e34979e8a7ca4df1e09da432
Sergey Segeda <segeda823@mail.ru>
2018-12-22 15:09:39 +0300

    Add lodash-es functions

--- src/components/BlogData/index.tsx
+++ src/components/BlogData/index.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
 import { dataRoute } from "../../constants";
+import * as React from "react";
+import map from "lodash-es/map";
 
 export interface IBlogState {
   data: string[] | number[];
@@ -31,14 +32,11 @@ class BlogData extends React.Component<IBlogState> {
   public componentDidMount() {
     fetch(dataRoute, { method: "GET" })
       .then(res => res.json())
-      .then((data) => {
-        const normalizeData: INomalizeData = data.map(
-          (element: IElementType) => {
-            const { id, content, title } = element;
-            return { id, content: content.rendered, title: title.rendered };
-          }
-        );
-        // console.dir(normalizeData);
+      .then(data => {
+        const normalizeData = map(data, (element: IElementType) => {
+          const { id, content, title } = element;
+          return { id, content: content.rendered, title: title.rendered };
+        });
         this.setState({ data: normalizeData });
       });
   }
@@ -51,7 +49,7 @@ class BlogData extends React.Component<IBlogState> {
             <th>Заголовок</th>
             <th>Текст</th>
           </tr>
-          {this.state.data.map((element: INomalizeData) => {
+                {map(this.state.data, (element: INomalizeData) => {
             return (
               <tr key={element.id}>
                 <td>{element.title}</td>
