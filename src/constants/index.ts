9a534ecdda96336353526830c1acce4e2be29f6c
Sergey Segeda <segeda823@mail.ru>
2018-12-24 08:53:35 +0300

    Add fetch constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,9 @@
 const perPage = 100;
-const categories = "5";
-const dataRoute = `https://smartfreelancer.ru//wp-json/wp/v2/posts?per_page=${perPage}&categories=${categories}`;
+const portfolioCategory = "5";
+const blogCategory = "3";
 
-export { dataRoute };
+const portfolioRoute = `https://smartfreelancer.ru//wp-json/wp/v2/posts?per_page=${perPage}&categories=${portfolioCategory}`;
+
+const blogRoute = `https://smartfreelancer.ru//wp-json/wp/v2/posts?per_page=${perPage}&categories=${blogCategory}`;
+
+export { portfolioRoute, blogRoute };
