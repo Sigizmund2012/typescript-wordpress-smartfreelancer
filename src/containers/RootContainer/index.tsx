852922c54f188a78bbd89afe11383b00a3603e87
Sergey Segeda <segeda823@mail.ru>
2018-12-30 12:23:23 +0300

    Add pages components to router

--- src/containers/RootContainer/index.tsx
+++ src/containers/RootContainer/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PortfolioData from "../../components/PortfolioData";
 import BlogData from "../../components/BlogData";
+import AboutMe from "../../components/AboutMe";
+import Contacts from "../../components/Contacts";
 
 const AppRouter = () => (
   <Router>
@@ -14,11 +16,19 @@ const AppRouter = () => (
           <li>
             <Link to="/blog/">Блог</Link>
           </li>
+          <li>
+            <Link to="/about-me/">Обо мне</Link>
+          </li>
+          <li>
+            <Link to="/contacts/">Контакты</Link>
+          </li>
         </ul>
       </nav>
 
       <Route path="/" exact component={PortfolioData} />
       <Route path="/blog/" component={BlogData} />
+      <Route path="/about-me/" component={AboutMe} />
+      <Route path="/contacts/" component={Contacts} />
     </div>
   </Router>
 );
