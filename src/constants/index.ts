fba5bf8901d99bab5520231ffe9fc2ebd976913e
Sergey Segeda <segeda823@mail.ru>
2018-12-30 12:19:18 +0300

    Add pages constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,15 @@
 const perPage = 100;
 const portfolioCategory = "5";
 const blogCategory = "3";
+const aboutMePage = "1729";
+const contactsPage = "1731";
 
-const portfolioRoute = `https://smartfreelancer.ru//wp-json/wp/v2/posts?per_page=${perPage}&categories=${portfolioCategory}`;
+const portfolioRoute = `https://smartfreelancer.ru/wp-json/wp/v2/posts?per_page=${perPage}&categories=${portfolioCategory}`;
 
-const blogRoute = `https://smartfreelancer.ru//wp-json/wp/v2/posts?per_page=${perPage}&categories=${blogCategory}`;
+const blogRoute = `https://smartfreelancer.ru/wp-json/wp/v2/posts?per_page=${perPage}&categories=${blogCategory}`;
 
-export { portfolioRoute, blogRoute };
+const aboutMePageRoute = `https://smartfreelancer.ru/wp-json/wp/v2/pages/${aboutMePage}`;
+
+const contactsPageRoute = `https://smartfreelancer.ru/wp-json/wp/v2/pages/${contactsPage}`;
+
+export { portfolioRoute, blogRoute, aboutMePageRoute, contactsPageRoute };
